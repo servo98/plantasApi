7c9f84c8dab99f0a09bb76a10ea563ad46347e50
Fernando Servín <tarzanboycraft@hotmail.com>
2020-01-14 16:05:06 -0600

    dot env with port

--- server.mjs
+++ server.mjs
@@ -1,5 +1,9 @@
 import {app} from './api/app'
 
-app.listen(3000, () => {
-    console.log('runing')
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Plants Server Running on port ${process.env.PORT}`)
 })
