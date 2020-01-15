c169f68935deb4e0d3f155c7f103b12e771ce010
Fernando Servín <tarzanboycraft@hotmail.com>
2020-01-15 15:53:04 -0600

    auth middleware

--- api/app.mjs
+++ api/app.mjs
@@ -5,9 +5,14 @@ import express from 'express'
 import * as db from './utils/db' 
 import dotenv from 'dotenv'
 
+/**
+ * Middlewares
+ */
+import * as auth from './middlewares/auth'
+
 
 /**
- * Routes
+ * Routes imports
  */
 import UserRoutes from './routes/UserRoutes';
 
@@ -15,11 +20,32 @@ dotenv.config();
 
 db.connect(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
 
+/**
+ * APP
+ */
 const app = express();
+app.use(express.json());
 
-app.use('/users', UserRoutes)
 
+/**
+ * App routes
+ */
+app.use('/users', auth.verifyAuth, UserRoutes)
+
+
+/**
+ * TESTS
+ */
 app.get('/', (req, res) => {
     res.send('hola');
 });
+
+
+/**
+ * 
+ */
+
+app.use(function (_, res, __) {
+    res.status(404).send("Data not found!")
+})
 export {app}
--- api/utils/auth.mjs
+++
@@ -1,2 +0,0 @@
-import jwt from 'jwt-simple'
-
