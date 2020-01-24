7883d14bf7dec677efc2e7d0a992cae58c9bacd4
Servo MacPro <tarzanboycraft@hotmail.com>
2020-01-23 20:47:32 -0600

    added middleware to show user

--- api/middlewares/auth.mjs
+++ api/middlewares/auth.mjs
@@ -7,9 +7,9 @@ export function verifyAuth(req, res, next) {
     try {
         const payload = jwt.decode(req.headers.auth, process.env.JWT_TOKEN_SECRET);
         //expira
-        if(payload.exp) {
-            return res.status(401).send({message: 'Token expired'})
-        }
+        // if(payload.exp) {
+        //     return res.status(401).send({message: 'Token expired'})
+        // }
         
     } catch (error) {
         return res.status(404).send({message: 'Invalid token'})
