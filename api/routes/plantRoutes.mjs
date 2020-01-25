d6a298fef6437c4023e861fad4796bdbc9269a22
Servo MacPro <tarzanboycraft@hotmail.com>
2020-01-25 15:26:37 -0600

    rotues plants

--- api/routes/plantRoutes.mjs
+++ api/routes/plantRoutes.mjs
@@ -1,10 +1,14 @@
 import express from 'express'
 import * as PlantController from '../controllers/PlantController.mjs'
 const api = express.Router();
-api.get('/', PlantController.index);
-api.get('/:id', PlantController.show)
-api.post('/', PlantController.create);
-api.put('/:id', PlantController.update)
-api.delete('/:id', PlantController.destroy)
+
+api.route('/')
+    .get('/', PlantController.index)
+    .post('/', PlantController.create);
+
+api.route('/:id')
+    .get(PlantController.show)
+    .put(PlantController.update)
+    .delete(PlantController.destroy)
 
 export default api;
