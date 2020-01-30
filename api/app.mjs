303159fe32a3424e6cfaf7efd8fe47005d6875b6
Servo MacPro <tarzanboycraft@hotmail.com>
2020-01-29 20:30:04 -0600

    coors

--- api/app.mjs
+++ api/app.mjs
@@ -23,6 +23,20 @@ db.connect(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME, proces
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
 /**
  * App routes
