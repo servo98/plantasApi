446a1be612608812fb64ab8c0032db06214a21c9
Servo MacPro <tarzanboycraft@hotmail.com>
2020-02-01 16:05:32 -0600

    another plant

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import {app} from './api/app.mjs'
 
 app.listen(process.env.PORT, () => {
-    console.log(`🌱 🌿 🌱 🌿 🌱 Plants Server Running on port ${process.env.PORT}`);
+    console.log(`🌱 🌱 🌿 🌱 🌿 🌱 Plants Server Running on port ${process.env.PORT}`);
 })
