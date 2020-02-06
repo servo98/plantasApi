09940bc4d8a0da4c8738c8af7748de56e2bc5f72
Servo MacPro <tarzanboycraft@hotmail.com>
2020-02-05 19:13:00 -0600

    to await an· ¨

--- api/controllers/AuthController.mjs
+++ api/controllers/AuthController.mjs
@@ -2,22 +2,22 @@ import User from '../models/UserModel.mjs'
 import jwt from 'jwt-simple'
 import bcrypt from 'bcrypt'
 
-export function login(req, res) {
-    User.findOne({ "email": req.body.email })
-        .then(user => {
-            if (!user)
-                return res.status(404).send({ message: 'Credenciales inválidas' });
+export async function login(req, res) {
+    try {
+        const user = await User.findOne({ "email": req.body.email });
+        if(!user)
+            return res.status(404).send({ message: 'Credenciales inválidas' }); 
+        
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            const token = jwt.encode({ id: user.id }, process.env.JWT_TOKEN_SECRET);
+            return res.send({ token, userId: user._id });
+        } 
+        
+        return res.status(403).send({ message: 'Credenciales inválidas' });
 
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                const token = jwt.encode({ id: user.id }, process.env.JWT_TOKEN_SECRET);
-                return res.send({ token, userId: user._id });
-            } 
-            
-            return res.status(403).send({ message: 'Credenciales inválidas' });
-        })
-        .catch(err => {
-            return res.status(500).send({message: 'Error al hacer login', err});
-        });
+    } catch(error) {
+        return res.send({message: 'Error al hacer login', error})
+    }
 }
 
 export function logout(req, res) {
--- api/controllers/PlantController.mjs
+++ api/controllers/PlantController.mjs
@@ -1,28 +1,28 @@
 import Plant from '../models/PlantModel.mjs'
 
-export function index(__, res) {
-    Plant.find({})
-        .then(plants => {
-            return res.send({plants});
-        })
-        .catch(err => {
-            return res.status(500).send({message: 'Error al buscar plantas', err});
-        });
+export async function index(__, res) {
+    try {
+        const plants = await Plant.find({});
+        if(!plants)
+            return res.status(404).send({message: 'No hay plantas'});
+        return res.send({plants});
+    } catch(error) {
+        return res.status(500).send({message: 'Error al buscar plantas', error})
+    }
 }
 
-export function show(req, res) {
-    Plant.findOne({_id: req.params.id})
-        .then(plant => {
-            if(!plant)
-                return res.status(404).send({message: 'Planta no encontrada'})
-            return res.send({plant})
-        })
-        .catch(err => {
-            return res.status(500).send({message: 'Error al buscar planta'});
-        });
+export async function show(req, res) {
+    try {
+        const plant = await Plant.findOne({_id: req.params.id});
+        if(!plant)
+            return res.status(404).send({message: 'Planta no encontrada'});
+        return res.send({plant});
+    } catch(error) {
+        return res.status(500).send({message: 'Error al mostrar planta', error});
+    }
 }
 
-export function create(req, res) {
+export async function create(req, res) {
     const newPlant = new Plant({
         name: req.body.name,
         specie: req.body.specie,
@@ -30,37 +30,29 @@ export function create(req, res) {
         size: req.body.size,
         price: req.body.price,
     });
-    newPlant.save()
-        .then(plant => {
-            return res.send({plant})
-        })
-        .catch(err => {
-            return res.status(500).send({message: 'Error al crear nueva planta', err});
-        });
+    try {
+        const plant = await newPlant.save();
+        return res.send({plant});
+
+    } catch(error) {
+        return res.status(500).send({message: 'Error al crear nueva planta', error});
+    }
 }
 
-export function update(req, res) {
-    Plant.findOneAndUpdate({"_id":req.params.id},
-        req.body,
-        {
-            new: true
-        },
-    (err, plant) => {
-        if(err){
-            res.status(500).send("error")
-       }else{
-            res.send({plant})
-       }
-   })
+export async function update(req, res) {
+    try {
+        const plant = await Plant.findOneAndUpdate({"_id":req.params.id}, req.body, { new: true })
+        return res.send({plant});
+    } catch(error) {
+        return res.status(500).send({message: 'Error al actualizar planta', error});
+    }
 }
 
-export function destroy(req, res){
-    Plant.findOneAndDelete({"_id":req.params.id},req.body,
-    (err, deleted) => {
-        if(err){
-            res.status(500).send("error");
-        }else{
-            res.send({message: 'Planta borrada exitosamente', deleted});
-        }
-   })
+export async function destroy(req, res){
+    try {
+        const plant = await Plant.findOneAndDelete({"_id":req.params.id},req.body);
+        return res.send({plant});
+    } catch (error) {
+        return res.status(500).send({message: 'Error al borrar planta', error});
+    }
 }
